4a29ba9116c01cc44d9611c7a9c377d578875af6
shivanagoudagooli <shivanagoudagooli@gmail.com>
2018-09-16 11:03:22 +0530

    added createtask component

--- testapp/src/app/app.module.ts
+++ testapp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreatetaskComponent } from './createtask/createtask.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatetaskComponent
   ],
   imports: [
     BrowserModule
